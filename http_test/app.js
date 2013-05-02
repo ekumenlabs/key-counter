26bec9224b6709d3c3080fb001a35d58c732b9bf
Elvio Toccalino <etoccalino@fi.uba.ar>
2013-05-02 01:58:16 -0300

    adapted HTTP demo app to use kpm little graphs.

--- http_test/app.js
+++ http_test/app.js
@@ -19,25 +19,60 @@ app.use(express.bodyParser());
 
 var allowedUserData = ['etoccalino'];
 
-app.post('/counts', function (req, res) {
-  // console.log('body: ' + JSON.stringify(req.body));
-  var collected = [];
-
-  collected = req.body;
-  // for (int i = 0; i < req.body; i++) {
-  //   var user = req.body[i].user
-  //     , count = req.body[i].count;
-
-  //   if (allowedUserData.indexOf(user) == -1) {
-  //     console.log('Data for user ' + user + ' is not allowed.');
-  //     res.send(400, 'Bad Request');
+var collected_ = {};
+function getCollected () {
+  var clone = {};
+  for (k in collected_) {
+    if (collected_.hasOwnProperty(k)) {
+      clone[k] = collected_[k];
+    }
+  }
+  return clone;
+}
+function resetCollected (newCollected) {
+  collected_ = newCollected;
+  // for (k in newCollected) {
+  //   if (newCollected.hasOwnProperty(k)) {
+  //     collected_[k] = newCollected[k];
   //   }
-  //   collected.push({username: user, count: count});
   // }
+}
 
-  // Responde to key_counter_server.
+// Collect data as it comes
+app.post('/counts', function (req, res) {
+  var user, value, collected = getCollected();
+  for (var i = 0; i < req.body.length; i++) {
+    if (allowedUserData.indexOf(req.body[i].username) != -1) {
+      user = req.body[i].username;
+      value = req.body[i].count;
+
+      collected[user] = collected[user] || [];
+      collected[user].push(value);
+    }
+  }
+  // Set the collected values for the app to see.
+  resetCollected(collected);
+  // Answer to key_counter_server.
   res.send(202, 'Accepted');
-
-  // Emit to connected clients to update their counts.
-  io.of('/key-count').emit('users update', collected);
 });
+
+var PUSH_INTERVAL = 5000;
+setInterval(function () {
+  var collected, value, data = [];
+
+  // Get a copy of the currently collected counts.
+  collected = getCollected();
+  // Reset the collected values.
+  resetCollected({});
+
+  // Reformat the collected data.
+  for (user in collected) {
+    // Average the collected counts.
+    for (var i = 0, value = 0; i < collected[user].length; i++) {
+      value = value + collected[user][i];
+    }
+    data.push({username: user, count: value / collected[user].length});
+  }
+  // Emit to connected clients to update their counts.
+  io.of('/key-count').emit('users update', data);
+}, PUSH_INTERVAL);
